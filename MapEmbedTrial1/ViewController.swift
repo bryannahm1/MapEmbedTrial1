ccdf70acc0ffe115bce8fb442ba7c33cb0646ec3
Bry <bryannahm1@gmail.com>
2022-08-04 11:56:27 -0700

    Initial Commit

---
+++ MapEmbedTrial1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapEmbedTrial1
+//
+//  Created by scholar on 8/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
